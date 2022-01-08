1ee2e35ae87bd49538ec67811470f32c5514c870
shinzoqchiuq <shinzoqchiuq@outlook.com>
2022-01-08 12:05:08 +0800

    fix(revise_dictionary): drop 篩選 == #

--- script/revise_dictionary.py
+++ script/revise_dictionary.py
@@ -11,14 +11,14 @@ import numpy as np
 from gninpou import compat2gninpou
 
 sheet = pd.read_csv(
-    open("../字表.tsv", encoding="utf-8"), sep="\t", comment="#",
+    open("../字表.tsv", encoding="utf-8"), sep="\t",
     usecols=["繁體", "简体", "兼容格式", "篩選", "備註"]
 )
 sheet.rename(columns={"兼容格式":"吳拼"}, inplace=True)
 
 sheet.drop(
     (
-        (sheet["篩選"] == "#") |
+        (sheet["繁體"] == "#") | (sheet["篩選"] == "#") |
         [type(i) != str for i in iter(sheet["吳拼"])]
     ).to_numpy().nonzero()[0]
     , inplace=True
@@ -35,4 +35,3 @@ sheet.reset_index(drop=True, inplace=True)
 sheet[["繁體", "简体", "吳拼", "備註"]].to_csv(
     "../各地字表/甬城（考證）.tsv", encoding="utf-8", index=False, sep="\t"
 )
-
